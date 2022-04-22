ed6fdb21e353a6e9dfa833755a57eada5194e2d1
Aitor <aitorrodriguez.daw@ciudadescolarfp.es>
2022-04-22 13:49:12 +0200

    Constructor

--- Ejercicio/src/Alumno.java
+++ Ejercicio/src/Alumno.java
@@ -4,4 +4,12 @@ public class Alumno {
 	private int dni;
 	private String nombre;
 	private String apellido;
+	public Alumno(int dni, String nombre, String apellido) {
+		
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+	
+	
 }
